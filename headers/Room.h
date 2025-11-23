aede5ee781f917a2a6170ff9934797320b48775b
icsd3 <urobertanton@gmail.com>
2025-11-24 01:29:57 +0200

    Create Room.h

---
+++ headers/Room.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../headers/Enemies.h"
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Room
+{
+    std::vector<Object> walls;
+    // std::vector<Object> obstacles;
+    std::vector<Enemy> enemies; 
+    
+
+public:
+
+};
